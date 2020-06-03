83fa1168c0ccacf564064527b7eb6c5610cceef6
dranzerAvi <aviralhtc2015@gmail.com>
2020-06-03 16:21:04 +0530

    3 June 4:20

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,260 @@
+import 'dart:io';
+
+import 'package:deliveryowner/upload_image.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(NewApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class NewApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHomePage2());
+  }
+}
+
+class MyHomePage2 extends StatefulWidget {
+  @override
+  _MyHomePage2State createState() => _MyHomePage2State();
+}
+
+class _MyHomePage2State extends State<MyHomePage2> {
+  String textValue = "Hello World!";
+  FirebaseMessaging firebaseMessaging = new FirebaseMessaging();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('New App'),
+        ),
+        body: Center(
+          child: Text(textValue),
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: SingleChildScrollView(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Register new product ",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w200,
+                      fontSize: 30,
+                      fontFamily: 'Roboto',
+                      fontStyle: FontStyle.italic)),
+              RegisterPet(),
+            ]),
+      )),
+    );
+  }
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class RegisterPet extends StatefulWidget {
+  RegisterPet({Key key}) : super(key: key);
+
+  @override
+  _RegisterPetState createState() => _RegisterPetState();
+}
+
+class _RegisterPetState extends State<RegisterPet> {
+  File imageFile;
+  final _formKey = GlobalKey<FormState>();
+  final listOfCategories = ["Clothes", "Daily needs"];
+  String dropdownValue = 'Clothes';
+  final nameController = TextEditingController();
+  final urlController = TextEditingController();
+  final ageController = TextEditingController();
+  final dbRef = FirebaseDatabase.instance.reference();
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+            child: Column(children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: TextFormField(
+              controller: nameController,
+              decoration: InputDecoration(
+                labelText: "Enter Product Name",
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+              // The validator receives the text that the user has entered.
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Enter Product Name';
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: TextFormField(
+              controller: urlController,
+              decoration: InputDecoration(
+                labelText: "Enter image url",
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+              // The validator receives the text that the user has entered.
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Enter image url';
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: DropdownButtonFormField(
+              value: dropdownValue,
+              icon: Icon(Icons.arrow_downward),
+              decoration: InputDecoration(
+                labelText: "Select Category",
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+              items: listOfCategories.map((String value) {
+                return new DropdownMenuItem<String>(
+                  value: value,
+                  child: new Text(value),
+                );
+              }).toList(),
+              onChanged: (String newValue) {
+                setState(() {
+                  dropdownValue = newValue;
+                });
+              },
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please Select Categpry';
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: TextFormField(
+              keyboardType: TextInputType.number,
+              controller: ageController,
+              decoration: InputDecoration(
+                labelText: "Enter product price",
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+              // The validator receives the text that the user has entered.
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter product price';
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+              padding: EdgeInsets.all(20.0),
+              child: RaisedButton(
+                color: Colors.lightBlue,
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    dbRef.child(dropdownValue).child(nameController.text).set({
+                      "Name": nameController.text,
+                      "Price": ageController.text,
+                      "ImageUrl": urlController.text,
+                    }).then((_) {
+                      Scaffold.of(context).showSnackBar(
+                          SnackBar(content: Text('Successfully Added')));
+                      ageController.clear();
+                      nameController.clear();
+                    }).catchError((onError) {
+                      Scaffold.of(context)
+                          .showSnackBar(SnackBar(content: Text(onError)));
+                    });
+                  }
+                },
+                child: Text('Submit'),
+              )),
+          Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  RaisedButton(
+                    color: Colors.lightBlue,
+                    onPressed: () {
+//                      Navigator.push(
+//                          context,
+//                          MaterialPageRoute(
+//                              builder: (context) => UploadImage()));
+                      _openImagePicker();
+                    },
+                    child: Text('Upload Image'),
+                  ),
+                ],
+              )),
+        ])));
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  void dispose() {
+    super.dispose();
+    ageController.dispose();
+    nameController.dispose();
+  }
+
+  void _openImagePicker() async {
+    File pick = await ImagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      print(' Attatchment- ${pick.path}');
+      imageFile = pick;
+      final StorageReference firebaseStorageRef =
+          FirebaseStorage.instance.ref().child('hello.jpg');
+      final StorageUploadTask task = firebaseStorageRef.putFile(pick);
+      print(task.isSuccessful.toString());
+    });
+    String platformResponse;
+
+    try {
+      await ImagePicker.pickImage(source: ImageSource.gallery);
+      platformResponse = 'success';
+    } catch (error) {
+      platformResponse = error.toString();
+    }
+
+    if (!mounted) return;
+    print(platformResponse);
   }
 }
